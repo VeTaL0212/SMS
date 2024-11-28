ec56aac54412c928bb642b8eb3bc0fc5f4e34e21
Vitali <vitaly.u@mail.ru>
2024-11-28 15:38:32 +0300

    Добавлена проверка корректности номера с помощью регулярных выражений

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import requests
-from bottle import response
+import re
+
+from PIL.ImImagePlugin import number
+
+
+def validate_phone_number(phone_number):
+    pattern = r'^\d{11}$'
+    return bool(re.match(pattern, phone_number))
+
 
 user='VitaliY'
 password='VVitos134'
@@ -7,15 +15,21 @@ sender='python2024'
 receiver='79215955943'
 text = 'Hello world!'
 
-url=f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
-print(url)
-try:
-    response = requests.get(url)
-    print(response)
+if not validate_phone_number(receiver):
+    print("Ошибка! Некорректный номер телефона")
+else:
 
-    if response.status_code == 200:
-        print("Сообщение успешно отправлено!")
-    else:
-        print(f"Ошибка при отправке{response.status_code}")
-except Exception as e:
-    print(f"Непредвиденная ошибка: {e}")
+    url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
+    print(url)
+    try:
+        response = requests.get(url)
+        print(response)
+
+        if response.status_code == 200:
+            print("Сообщение успешно отправлено!")
+        else:
+            print(f"Ошибка при отправке{response.status_code}")
+    except Exception as e:
+        print(f"Непредвиденная ошибка: {e}")
+
+
