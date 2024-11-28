cd549324a68507e156a9d60f67d2b6c135e9eff4
Vitali <vitaly.u@mail.ru>
2024-11-28 16:54:17 +0300

    функционал получения баланса добавлен в основной блок программы

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 import requests
 import re
+import json
 
-from PIL.ImImagePlugin import number
+
+def check_balance(login, password):
+    url = 'https://my3.webcom.mobi/json/balance.php'
+    headers = {'Content-type': 'text/json; charset=utf-8;'}
+
+    data = {"login": login,
+            "password": password}
+    try:
+        response = requests.post(url, data=json.dumps(data), headers=headers)
+
+        if response.status_code == 200:
+            response_data = response.json()
+            print(response_data)
+            print(f"Баланс: {response_data['money']} руб.")
+        else:
+            print(f"Произошла ошибка {response.status_code}")
+    except Exception as e:
+        print(f"Произошла не предвиденная ошибка {e}")
 
 
 def validate_phone_number(phone_number):
@@ -15,21 +33,29 @@ sender='python2024'
 receiver='79215955943'
 text = 'Hello world!'
 
-if not validate_phone_number(receiver):
-    print("Ошибка! Некорректный номер телефона")
-else:
 
-    url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
-    print(url)
-    try:
-        response = requests.get(url)
-        print(response)
+balance = check_balance(user, password)
+if balance:
+    if balance > 10:
 
-        if response.status_code == 200:
-            print("Сообщение успешно отправлено!")
+        if not validate_phone_number(receiver):
+            print("Ошибка! Некорректный номер телефона")
         else:
-            print(f"Ошибка при отправке{response.status_code}")
-    except Exception as e:
-        print(f"Непредвиденная ошибка: {e}")
+            url = f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
+            print(url)
+            try:
+                response = requests.get(url)
+                print(response)
+
+                if response.status_code == 200:
+                    print("Сообщение успешно отправлено!")
+                else:
+                    print(f"Ошибка при отправке{response.status_code}")
+            except Exception as e:
+                print(f"Непредвиденная ошибка: {e}")
+    else:
+        print("Недостаточно средств!")
+else:
+    print("Не удалось получить информацию о балансе")
 
 
