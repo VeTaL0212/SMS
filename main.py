b683ab6331b0cd3083c1494dd915362bde533693
Vitali <vitaly.u@mail.ru>
2024-11-28 15:20:51 +0300

    Добавлена обработка исключений

--- main.py
+++ main.py
@@ -2,17 +2,20 @@ import requests
 from bottle import response
 
 user='VitaliY'
-passwrd='VVitos134'
+password='VVitos134'
 sender='python2024'
 receiver='79215955943'
 text = 'Hello world!'
 
 url=f"https://my3.webcom.mobi/sendsms.php?user={user}&pwd={password}&sadr={sender}&dadr={receiver}&text={text}"
 print(url)
-response = request.get(url)
-print(response)
+try:
+    response = requests.get(url)
+    print(response)
 
-if response.status_code == 200:
-    print("Сообщение успешно отправлено!")
-else:
-    print(f"Ошибка при отправке{response.status_code}")
+    if response.status_code == 200:
+        print("Сообщение успешно отправлено!")
+    else:
+        print(f"Ошибка при отправке{response.status_code}")
+except Exception as e:
+    print(f"Непредвиденная ошибка: {e}")
